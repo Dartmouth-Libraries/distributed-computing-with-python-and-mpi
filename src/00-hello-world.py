12291bbefa92065e686f5ffb931a230c82614d1a
Simon Stone <simon.stone@dartmouth.edu>
2023-03-01 08:45:46 -0500

    adds node name to output

--- src/00-hello-world.py
+++ src/00-hello-world.py
@@ -1,10 +1,12 @@
 # Import the mpi4py module (internally calls MPI_Init())
 from mpi4py import MPI
 
+import os
+
 # Get a handle of the `World` communicator
 comm = MPI.COMM_WORLD
 
 # Obtain the rank of this process in the `World` communicator
 rank = comm.Get_rank()
 
-print(f'Hello, world! I am rank {rank}!')
+print(f'Hello, world! I am rank {rank} running on {os.environ["SLURMD_NODENAME"]}!')
